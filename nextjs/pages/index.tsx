acfec681e1d748964d4453c3951a3895a2b8c5de
Rob Brennan <rob@therobbrennan.com>
2021-09-14 22:36:05 -0700

    Add REVISIT comment for us to review a known bug with the leaflet library and Safari on macOS

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -19,6 +19,11 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>{PAGE_TITLE}</h1>
 
+        {/* REVISIT: We need to address a known bug in Safari.
+            Safari behavior when clicking a pop-up using the latest release of Leaflet (1.7.1) only works if there is a long press.
+
+            See https://github.com/Leaflet/Leaflet/issues/7331 for more information
+        */}
         <Map className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
           {({ TileLayer, Marker, Popup }) => (
             <>
